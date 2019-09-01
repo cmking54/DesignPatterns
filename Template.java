8a002e1282b8e42ac93af6d74d8bb8762cf6d7dd
cmking54 <cmking5496@gmail.com>
2019-09-01 11:26:22 -0400

    Added Helper Files

---
+++ Template.java
@@ -0,0 +1,10 @@
+/* Name: Command
+Use: Representing and encapsualting all the info needed to call a method at a later time
+Theme Seed:
+*/
+public class CommandPattern {
+    public static void main(String[] args) {
+        
+    }
+}
+
